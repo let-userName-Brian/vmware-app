2602df1b647af85e92dde0a5eceafdbeeebdce5d
Brian <brian.hardy67@gmail.com>
2022-01-14 10:56:35 -0500

    initial layout/imports installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { ClarityModule } from '@clr/angular';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MainBodyComponent } from './main-body/main-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SideNavComponent,
+    MainBodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ClarityModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
